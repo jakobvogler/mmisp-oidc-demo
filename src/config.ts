c57afd96f307eff10b5ce0abb6ffdcb84ee1192f
Jakob Christoph Vogler <jakob@jcvogler.com>
2024-03-23 22:28:33 +0100

    feat: add config

---
+++ src/config.ts
@@ -0,0 +1,8 @@
+// import getConfig from "next/config"
+
+// const {publicRuntimeConfig} = getConfig()
+
+export function API_URL(): string {
+  return "http://localhost:4000"
+  // return publicRuntimeConfig.API_URL
+}
