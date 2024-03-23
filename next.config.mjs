a7501fc89bc2fd2a362543f353aaf874c64d055b
Jakob Christoph Vogler <jakob@jcvogler.com>
2024-03-23 23:04:24 +0100

    fix: disable nextjs optimization

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
+  },
   publicRuntimeConfig: {
     API_URL: process.env.API_URL,
   },
